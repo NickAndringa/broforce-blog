633297dffad1b6501187afd2774fec53dbc60c25
nickandringa <n.andringa@outlook.com>
2022-03-11 23:47:16 +0100

    Lesson 10 finished

--- src/Home.js
+++ src/Home.js
@@ -2,20 +2,20 @@ import './styles/_home.sass';
 import { useState} from "react";
 
 const Home = () => {
-    // let name= 'Bro';
-    const [name, setName] = useState('Bro');
-    const [age,setAge] = useState(20);
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'Lorem ipsum...',author: 'Nick', id: 1},
+        {title: 'Welcome party', body: 'Lorem ipsum...',author: 'Mario', id: 2},
+        {title: 'Web dev top tips', body: 'Lorem ipsum...',author: 'Yoshi', id: 3}
 
-    const handleClick = () =>{
-        setName('Nick')
-        setAge(22)
-    }
-
+    ]);
     return(
         <div className= 'Home' >
-            <h2 className= 'font-bold'> Homepage</h2>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}> Click me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview bg-stone-300 p-5 m-2 rounded-bl-3xl rounded-tr-3xl hover:bg-stone-400 transition-all hover:py-10" key={blog.id}>
+                    <h2 className= "text-2xl">{blog.title}</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
         </div>
 
     );
