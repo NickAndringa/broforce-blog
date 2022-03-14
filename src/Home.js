179ba2d951a44a44dc1d743e7a1da86531a3a639
nickandringa <n.andringa@outlook.com>
2022-03-14 20:25:48 +0100

    Lesson 13 finished

--- src/Home.js
+++ src/Home.js
@@ -8,9 +8,15 @@ const Home = () => {
         {title: 'Welcome party', body: 'Lorem ipsum...',author: 'Mario', id: 2},
         {title: 'Web dev top tips', body: 'Lorem ipsum...',author: 'Yoshi', id: 3}
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
+    }
+
     return(
         <div className= 'Home' >
-            <Bloglist blogs={blogs} title={'All blogs'} />
+            <Bloglist blogs={blogs} title={'All blogs'} handleDelete={handleDelete}/>
             <Bloglist blogs={blogs.filter((blog) => blog.author === 'Nick' )} title={"Nick's blogs"} />
         </div>
 
